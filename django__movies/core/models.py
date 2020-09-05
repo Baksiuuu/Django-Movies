62107d5dfc3238fddce710301c918397b8c24178
Andrzej Żygenda <zygendaandrzej@gmail.com>
2020-09-05 13:53:03 +0200

    Gitignore added. Core added.

---
+++ django__movies/core/models.py
@@ -0,0 +1,17 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+
+class Movie(models.Model):
+    title = models.CharField(max_lenght = 100)
+    rating = models.IntegerField(
+        null=True, validators = [MaxValueValidator(10), MinValueValidator(1)]
+    )
+
+    relased = models.DateField()
+    description = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title} from {self.relased}'
+
+# Create your models here.
