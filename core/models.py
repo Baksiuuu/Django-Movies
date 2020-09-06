85c45a99fc3404ccb00709f8a8bbe5cadf30f692
Andrzej Żygenda <zygendaandrzej@gmail.com>
2020-09-06 12:26:25 +0200

    Add unique for Director & Movie

--- core/models.py
+++ core/models.py
@@ -16,8 +16,12 @@ class Genre(models.Model):
         return f'{self.name} with age limit set on {self.age_limit}'
 
 class Director(models.Model):
-    first_name = models.CharField(max_length = 50)
-    last_name = models.CharField(max_length = 50)
+    first_name = models.CharField(max_length = 50, null = True, blank = True)
+    last_name = models.CharField(max_length = 50, null = True, blank = True)
+
+
+    class Meta:
+        unique_together = ('first_name', 'last_name')
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
@@ -33,6 +37,9 @@ class Movie(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     genre = models.ForeignKey(Genre, null = True, on_delete = models.SET_NULL)
     director = models.ForeignKey(Director, null = True, on_delete = models.SET_NULL)
+
+    class Meta:
+        unique_together = ('title', 'director', 'released')
 
     def __str__(self):
         return f'{self.title} from {self.released}'
