9d982880ac46b23dfcedf5d9b032c8336b8b5244
Andrzej Żygenda <zygendaandrzej@gmail.com>
2020-09-27 09:43:21 +0200

    Add SignUpForm

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from accounts.views import SubmittablePasswordChangeView, SuccessMessagedLogoutView, \
-    SubmittableLoginView
+    SubmittableLoginView, SignUpView
 
 app_name = 'accounts'
 urlpatterns = [
     path('login/', SubmittableLoginView.as_view(), name='login'),
     path('logout/', SuccessMessagedLogoutView.as_view(), name='logout'),
     path('passwordchange/', SubmittablePasswordChangeView.as_view(), name='password_change'),
+    path('register/', SignUpView.as_view(), name='register_user'),
 ]
